7dbf5f96ac16a6cc14f25b8e517890bd52940b0a
scalaview <binzhi2012@sina.com>
2015-07-20 16:09:08 +0800

    start

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
 var express = require('express');
-var wechat = require('wechat');
-var config = {
-  token: 'token',
-  appid: 'appid',
-  encodingAESKey: 'encodinAESKey'
-};
-
-app.use(express.query());
-
 var app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(6000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
+
