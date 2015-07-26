31d0dbb59396811669e8624e9e6e99a8d12e5864
benko <chailink100@gmail.com>
2015-07-26 11:31:04 +0800

    edit mennus

--- config.js
+++ config.js
@@ -4,34 +4,50 @@ var config = {
                 'aesKey': '48r29Vap81kFESsDcsaPXZOYfCm86G7mIBoLRUzIUbf',
                 'appSecret': '329e3398b76c96f9e044fb172ef1a3a8',
                 'menus': {
-                       "button":[
-                       {
-                            "type":"click",
-                            "name":"今日歌曲",
-                            "key":"V1001_TODAY_MUSIC"
-                        },
-                        {
-                             "name":"菜单",
-                             "sub_button":[
-                             {
-                                 "type":"view",
-                                 "name":"搜索",
-                                 "url":"http://www.soso.com/"
-                              },
+                           "button":[
+                           {
+                                "type":"click",
+                                "name":"玩流量",
+                                "sub_button":[
+                                {
+                                  "type":"view",
+                                  "name":"新手攻略",
+                                  "url":"http://www.yiliuliang.net"
+                                },
+                                {
+                                  "type":"view",
+                                  "name":"领取记录",
+                                  "url":"http://www.yiliuliang.net"
+                                },
+                                {
+                                  "type":"view",
+                                  "name":"领取流量",
+                                  "url":"http://www.yiliuliang.net"
+                                },
+                                {
+                                  "type":"click",
+                                  "name":"今日任务",
+                                  "key":"V1001_TODAY_TASKS"
+                                }]
+                            },
+                            {
+                                "type":"view",
+                                "name":"我的钱包",
+                                "url":"http://www.yiliuliang.net"
+                            },
+                            {
+                              "type":"view",
+                              "name":"更多",
+                              "sub_button":[
                               {
-                                 "type":"view",
-                                 "name":"视频",
-                                 "url":"http://v.qq.com/"
-                              },
-                              {
-                                 "type":"click",
-                                 "name":"赞一下我们",
-                                 "key":"V1001_GOOD"
+                                "type":"view",
+                                "name":"关于易流量",
+                                "url":"http://www.yiliuliang.net"
                               }]
-                         }]
-                   },
+                            }]
+                        },
                 'menus_keys': {
-                  'button1' :'V1001_TODAY_MUSIC'
+                  'button1' :'V1001_TODAY_TASKS'
                 }
               };
 
