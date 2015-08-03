ae0e04531d87a866289be4e26a97bf9e90d00564
eamon <eamonding@gmail.com>
2015-08-04 01:11:28 +0800

    Add requirejs

---
+++ public/javascript/main.js
@@ -0,0 +1,10 @@
+require(['jquery', 'bootstrap'], function ($) {
+
+});
+
+require.config({
+  paths: {
+    jquery: '../bower_components/jquery/dist/jquery.min',
+    bootstrap: '../bower_components/bootstrap/dist/js/bootstrap.min'
+  }
+});
