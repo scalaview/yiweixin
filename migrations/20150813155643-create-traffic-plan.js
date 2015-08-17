35b080eb4f77a8882634abd3ef72f9afea74392f
benko <chailink100@gmail.com>
2015-08-17 11:58:05 +0800

    edit init data

--- migrations/20150813155643-create-traffic-plan.js
+++ migrations/20150813155643-create-traffic-plan.js
@@ -52,6 +52,15 @@ module.exports = {
       queryInterface.sequelize.query("insert into TrafficPlans (providerId, name, value, cost, sortNum, createdAt, updatedAt) values (0, '500M', 500, 3000, 4, CURTIME(), CURTIME())");
       queryInterface.sequelize.query("insert into TrafficPlans (providerId, name, value, cost, sortNum, createdAt, updatedAt) values (0, '1024M', 1024, 5000, 5, CURTIME(), CURTIME())");
       queryInterface.sequelize.query("insert into TrafficPlans (providerId, name, value, cost, sortNum, createdAt, updatedAt) values (0, '2048M', 2048, 7000, 6, CURTIME(), CURTIME())");
+
+      queryInterface.sequelize.query("insert into TrafficPlans (providerId, name, value, cost, sortNum, createdAt, updatedAt) values (1, '30M', 30, 500, 0, CURTIME(), CURTIME())");
+      queryInterface.sequelize.query("insert into TrafficPlans (providerId, name, value, cost, sortNum, createdAt, updatedAt) values (1, '70M', 70, 1000, 1, CURTIME(), CURTIME())");
+      queryInterface.sequelize.query("insert into TrafficPlans (providerId, name, value, cost, sortNum, createdAt, updatedAt) values (1, '150M', 150, 2000, 2, CURTIME(), CURTIME())");
+      queryInterface.sequelize.query("insert into TrafficPlans (providerId, name, value, cost, sortNum, createdAt, updatedAt) values (1, '200M', 200, 1400, 3, CURTIME(), CURTIME())");
+      queryInterface.sequelize.query("insert into TrafficPlans (providerId, name, value, cost, sortNum, createdAt, updatedAt) values (1, '500M', 500, 3000, 4, CURTIME(), CURTIME())");
+      queryInterface.sequelize.query("insert into TrafficPlans (providerId, name, value, cost, sortNum, createdAt, updatedAt) values (1, '1024M', 1024, 5000, 5, CURTIME(), CURTIME())");
+      queryInterface.sequelize.query("insert into TrafficPlans (providerId, name, value, cost, sortNum, createdAt, updatedAt) values (1, '2048M', 2048, 7000, 6, CURTIME(), CURTIME())");
+
     });
   },
   down: function(queryInterface, Sequelize) {
