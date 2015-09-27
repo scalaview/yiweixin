c1b3aefdd3b9f45da7e5dcbe434dd4990588b384
benko <chailink100@gmail.com>
2015-09-27 12:56:49 +0800

    add dataplan

--- app.js
+++ app.js
@@ -1970,11 +1970,51 @@ admin.get('/syncdata', function(req, res) {
 
 
 admin.get('/dataplans', function(req, res) {
-  models.findAndCountAll().then(function(dataplans) {
-
+  models.DataPlan.findAndCountAll().then(function(dataplans) {
+    result = helpers.setPagination(dataplans, req)
+    res.render('admin/dataplans/index', { dataplans: result })
   }).catch(function(err) {
-
+    console.log(err)
+    res.redirect('/500')
   })
+})
+
+admin.get('/dataplans/:id/edit', function(req, res) {
+  models.DataPlan.findById(req.params.id).then(function(dataplan) {
+    res.render('admin/dataplans/edit', {
+      dataplan: dataplan,
+      path: '/admin/dataplan/' + dataplan.id
+    })
+  }).catch(function(err) {
+    console.log(err)
+    res.redirect('/500')
+  })
+})
+
+admin.post('/dataplan/:id', function(req, res) {
+  async.waterfall([function(next) {
+    models.DataPlan.findById(req.params.id).then(function(dataplan) {
+      next(null, dataplan)
+    }).catch(function(err) {
+      next(err)
+    })
+  }, function(dataplan, next) {
+    dataplan.updateAttributes(req.body).then(function(dataplan) {
+      next(null, dataplan)
+    }).catch(function(err) {
+      next(err)
+    })
+  }], function(err, dataplan) {
+    if(err){
+      console.log(err)
+      req.flash("info", "update fail")
+      res.redirect('/500')
+    }else{
+      req.flash("info", "update success")
+      res.redirect('/admin/dataplans/' + dataplan.id + "/edit")
+    }
+  })
+
 })
 
 // -------------- adming ---------------------
