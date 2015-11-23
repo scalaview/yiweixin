fbbcc06c2667c23a3088fbd8fcaf5300d6437131
scalaview <chailink100@gmail.com>
2015-11-23 12:07:22 +0800

    config edit

---
+++ routers/admin/dconfigs.js
@@ -0,0 +1,81 @@
+var express = require('express');
+var admin = express.Router();
+var models  = require('../../models')
+var helpers = require("../../helpers")
+var async = require("async")
+
+
+admin.get('/configs', function(req, res) {
+  models.DConfig.findAll({}).then(function(dconfigs){
+    var result = {}
+
+    for (var i = 0; i < dconfigs.length ; i++) {
+      if(dconfigs[i].name == 'vipLimit'){
+        result['vipLimit'] = {
+            name: "自动升级VIP限制",
+            key: 'vipLimit',
+            value: dconfigs[i].value
+        }
+      }
+
+      if(dconfigs[i].name == 'exchangeRate'){
+        result['exchangeRate'] = {
+          name: '兑换汇率(1元等于多少E币)',
+          key: 'exchangeRate',
+          value: dconfigs[i].value
+        }
+      }
+
+      // if(dconfigs[i].name == ''){
+
+      // }
+
+    };
+
+    res.render('admin/dconfigs/show', { result: result })
+
+  })
+})
+
+
+admin.post('/configs', function(req, res) {
+  models.DConfig.findAll({}).then(function(dconfigs) {
+
+    async.map(dconfigs, function(dconfig, next){
+      if(dconfig.name == 'vipLimit'){
+        dconfig.updateAttributes({
+          value: req.body.vipLimit
+        }).then(function(dconfig) {
+          next(null, dconfig)
+        }).catch(function(err) {
+          next(err)
+        })
+      }
+      if(dconfig.name == 'exchangeRate' ){
+        dconfig.updateAttributes({
+          value: req.body.exchangeRate
+        }).then(function(dconfig) {
+          next(null, dconfig)
+        }).catch(function(err) {
+          next(err)
+        })
+      }
+      if(dconfig.name ){
+
+      }
+
+    }, function(err, result) {
+      if(err){
+        console.log(err)
+        req.flash("err", "update fail")
+        res.redirect('/500')
+      }else{
+        req.flash("info", "update succes")
+        res.redirect('/admin/config')
+      }
+    })
+
+  })
+})
+
+module.exports = admin;
