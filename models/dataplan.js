24dc1ab28726d10a5fc28a378be756875c816518
benko <chailink100@gmail.com>
2015-11-29 11:22:26 +0800

    fix DECIMAL(10, 2)

--- models/dataplan.js
+++ models/dataplan.js
@@ -3,7 +3,14 @@ module.exports = function(sequelize, DataTypes) {
   var DataPlan = sequelize.define('DataPlan', {
     name: { type: DataTypes.STRING, allowNull: false },
     value: { type: DataTypes.INTEGER, allowNull: false },
-    price: { type: DataTypes.DECIMAL(10, 2), allowNull: true, defaultValue: 0 }
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: true,
+      defaultValue: 0,
+      set: function(val) {
+        this.setDataValue('price', parseFloat(val))
+      }
+   }
   }, {
     classMethods: {
       associate: function(models) {
