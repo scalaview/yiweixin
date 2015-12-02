09bbccfb2d681d21e376a2d4cd4cddae877bb929
benko <chailink100@gmail.com>
2015-12-02 21:42:02 +0800

    change api

--- routers/yiweixin/wechat.js
+++ routers/yiweixin/wechat.js
@@ -8,7 +8,19 @@ var OAuth = require('wechat-oauth');
 var config = require("../../config")
 var wechat = require('wechat')
 var WechatAPI = require('wechat-api');
-var api = new WechatAPI(config.appId, config.appSecret);
+// var api = new WechatAPI(config.appId, config.appSecret);
+
+var api = new WechatAPI(config.appId, config.appSecret, function (callback) {
+  // 传入一个获取全局token的方法
+  fs.readFile(process.env.PWD + '/access_token.txt', 'utf8', function (err, txt) {
+    if (err) {return callback(err);}
+    callback(null, JSON.parse(txt));
+  });
+}, function (token, callback) {
+  // 请将token存储到全局，跨进程、跨机器级别的全局，比如写到数据库、redis等
+  // 这样才能在cluster模式及多机情况下使用，以下为写入到文件的示例
+  fs.writeFile(process.env.PWD + '/access_token.txt', JSON.stringify(token), callback);
+});
 
 var maxDepth = config.max_depth
 
