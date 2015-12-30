9a9fb3013c0e27d0d12661d67a27fd4fe1535c13
scalaview <chailink100@gmail.com>
2015-12-30 15:54:58 +0800

    add env adjust

--- helpers/index.js
+++ helpers/index.js
@@ -491,7 +491,9 @@ function errTips(err) {
 // ===================login=====================
 
 function requireLogin(req, res, next) {
-  // req.session.customer_id = 1
+  if(process.env.NODE_ENV == "development"){
+    req.session.customer_id = 1
+  }
   var url = req.originalUrl
   var encodeUrl = new Buffer(url).toString('base64');
 
